3be2b32929ef2d92f3857e4f64c44b530f761b27
danieltyd <danieltyd81@gmail.com>
2018-03-28 09:58:15 -0500

    Generar error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent {
   mng=false;
   empleados=[
     {'name':'nombre1',cargo:'obrero'},
-    {'name':'nombre2',cargo:'Gerente'},
-    {'name':'nombre3',cargo:'Programado'}
+    {'name':'nombre2',cargo:'Gerente'}
+    
   ];
   model:any ={};
   constructor()
